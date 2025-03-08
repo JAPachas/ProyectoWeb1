9106441c2602fa6b364e2683f2df346b658d5172
Jhon Pachas <jhonpachasnapa@gmail.com>
2025-03-07 23:13:32 -0500

    Se cambio el nombre de los botones de navegación

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -18,10 +18,10 @@ const Navigator = () =>{
                 border: '1px solid #e0e0e0', 
                 width: '356px',             
                 }}>                    
-                <MyButton text="Home" ColorFondo="#f6f6f6" CornerRadius={0} fontColor="#656565" flexNumber={1} hover="#e0e0e0" FontFamily="Raleway"/>
-                <MyButton text="Portfolio" ColorFondo="#f6f6f6" CornerRadius={0} fontColor="#656565" flexNumber={1} hover="#e0e0e0" FontFamily="Raleway"/>
-                <MyButton text="Contact" ColorFondo="#f6f6f6" CornerRadius={0} fontColor="#656565" flexNumber={1} hover="#e0e0e0" FontFamily="Raleway"/>
-                <MyButton text="Weddings" ColorFondo="#f6f6f6" CornerRadius={0} fontColor="#656565" flexNumber={1} hover="#e0e0e0" FontFamily="Raleway"/>
+                <MyButton text="HOME" ColorFondo="#f6f6f6" CornerRadius={0} fontColor="#656565" flexNumber={1} hover="#e0e0e0" FontFamily="Raleway"/>
+                <MyButton text="ABOUT" ColorFondo="#f6f6f6" CornerRadius={0} fontColor="#656565" flexNumber={1} hover="#e0e0e0" FontFamily="Raleway"/>
+                <MyButton text="BLOG" ColorFondo="#f6f6f6" CornerRadius={0} fontColor="#656565" flexNumber={1} hover="#e0e0e0" FontFamily="Raleway"/>
+                <MyButton text="CONTACT" ColorFondo="#f6f6f6" CornerRadius={0} fontColor="#656565" flexNumber={1} hover="#e0e0e0" FontFamily="Raleway"/>
             </Box>
         </Box>
     );
